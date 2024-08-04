e17a44f42a811d23ad4a34455a72a50fe20e6ea5
Nikhil Sharma <nikhils.sharma2409@gmail.com>
2024-08-04 21:54:13 +0530

    update user data page added

--- index.js
+++ index.js
@@ -1,45 +1,61 @@
-const express =require('express');
-const app=express();
-const path=require('path');
-const usermodel=require('./models/user')
+const express = require('express');
+const app = express();
+const path = require('path');
+const usermodel = require('./models/user')
 
-app.set("view engine","ejs");
+app.set("view engine", "ejs");
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 
 
-app.get('/',function (req,res) {
+app.get('/', function (req, res) {
     res.render("index.ejs");
-    
+
 })
-app.get('/delete/:id',async function (req,res) {
-    let todelete= await usermodel.findOneAndDelete({_id: req.params.id});
+app.get('/delete/:id', async function (req, res) {
+    let todelete = await usermodel.findOneAndDelete({ _id: req.params.id });
     res.redirect('/user');
 })
-app.get('/edit/:id',async function (req,res) {
-    let to_edit= await usermodel.find ({_id: req.params.id});
-    res.render('edit.ejs',{to_edit});
-})
-app.get('/user',async function (req,res) {
-    let allusers= await usermodel.find();
+
+
+app.get('/edit/:id', async (req, res) => {
+    let to_edit = await usermodel.findById(req.params.id);
+    res.render('edit', { to_edit });
+});
+app.post('/update/:id', async (req, res) => {
+    let updated_data= await usermodel.findOneAndUpdate({ _id: req.params.id }, {
+        names: req.body.names,
+        email: req.body.email,
+        password: req.body.password,
+
+    }, { new: true },
+    // console.log(updated_data),
+   res.redirect("/user")
+)
+});
+
+
+
+app.get('/user', async function (req, res) {
+    let allusers = await usermodel.find();
     console.log(allusers);
-    res.render("seeuser.ejs",{users:allusers});
-    
+    res.render("seeuser.ejs", { users: allusers });
+
 })
 
-app.post('/create',async function (req,res) {
+app.post('/create', async function (req, res) {
     // let {name,email,password}=req.body;
-    let createduser=await usermodel.create(
+    let createduser = await usermodel.create(
         {
-             names:req.body.names,
-             email:req.body.email,
-             password:req.body.password,
-            
+            names: req.body.names,
+            email: req.body.email,
+            password: req.body.password,
+
         }
-)
+    )
 
     res.redirect('/user');
 })
